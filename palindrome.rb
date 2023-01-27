fe84a6a6b6e6688ac86b7a193da003092c7587e8
Raj Kumar <rajkumar@vebuin.com>
2023-01-27 21:04:08 +0530

    palindrome

---
+++ palindrome.rb
@@ -0,0 +1,10 @@
+print " enter text for palindrome string "
+ user =gets.chomp
+ 
+ if user.reverse ==user
+ print "It is a palindrome string"
+ puts
+ else 
+ print "It is not a palindrome string"
+ puts
+ end
